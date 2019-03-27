b2622c472cc41f20a8a500e6622988fce0ed9674
chloelns <chloe.lenain9106@gmail.com>
2019-03-27 18:58:38 +0100

    initial commit

---
+++ src/main/java/io/github/chloelns/projet/apajh/ProjetApajhApplication.java
@@ -0,0 +1,13 @@
+package io.github.chloelns.projet.apajh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetApajhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetApajhApplication.class, args);
+    }
+
+}
